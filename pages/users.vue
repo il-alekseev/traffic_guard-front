0e2cb20d2ab975b67bfe9762ca6d810668984956
rowanex <artem23klyukin@gmail.com>
2025-11-27 19:03:27 +0500

    feature: update users page. remove ids. divide role and ngfw in table

--- pages/users.vue
+++ pages/users.vue
@@ -46,6 +46,21 @@
         <span class="users__login-cell">{{ value }}</span>
       </template>
 
+      <template #cell-role="{ value }">
+        <span class="users__role-cell">{{ getRoleDisplayName(value) }}</span>
+      </template>
+
+      <template #cell-context="{ item }">
+        <span
+          class="users__table-cell__badge users__context-cell"
+          v-bind="getOnlyRole(item.role) !== 'SA' 
+                  ? { style: generateColor(getOnlyDeviceName(item.role)) } 
+                  : { style: {background: 'transparent'} }"
+        >
+          {{ getOnlyDeviceName(item.role) || '' }}
+        </span>
+      </template>
+
       <template #cell-full_name="{ item }">
         {{ item.last_name }} {{ item.first_name }} {{ item.patronymic || '' }}
       </template>
@@ -112,12 +127,16 @@ import ErrorBlock from '~/components/ui/ErrorBlock.vue';
 import BaseTable from '~/components/ui/BaseTable.vue';
 import PlusIcon from "~/assets/img/plus.svg";
 import EditDataIcon from '~/assets/img/edit.svg';
+import { getRoleDisplayName, getOnlyRole, getOnlyDeviceName } from "~/helpers";
+import { useDeviceColors } from '~/composables/useDeviceColors';
 
 
 definePageMeta({
   layout: 'dashboard',
   middleware: ['auth']
 });
+
+const { generateColor } = useDeviceColors();
 
 const route = useRoute();
 const router = useRouter();
@@ -137,9 +156,9 @@ const tempPasswordType = ref<'create' | 'reset'>('create')
 
 const users = ref<User[]>([]);
 const columns = [
-  { key: 'user_id', label: 'ID' },
   { key: 'login', label: 'Логин' },
   { key: 'role', label: 'Роль' },
+  { key: 'context', label: 'Контекст' },
   { key: 'full_name', label: 'ФИО' },
   { key: 'email', label: 'Email' }
 ]
@@ -362,23 +381,32 @@ watch(
   color: #2563EB;
 }
 
-:deep(.users__table-column-user_id) {
-  width: 10%;
+:deep(.users__table-cell__badge) {
+  font-weight: 500;
+  font-size: 0.75rem;
+  line-height: 1rem;
+  padding: 0.25rem 0.5rem;
+  text-transform: uppercase;
+  border-radius: 6px;
 }
+
 :deep(.users__table-column-login) {
-  width: 15%
-}
-:deep(.users__table-column-role) {
   width: 10%
 }
+:deep(.users__table-column-role) {
+  width: 17.5%
+}
+:deep(.users__table-column-context) {
+  width: 17.5%
+}
 :deep(.users__table-column-full_name) {
-  width: 27%;
+  width: 25%;
 }
 :deep(.users__table-column-email) {
-  width: 28%;
+  width: 25%;
 }
 :deep(.users__table-column-button) {
-  width: 10%;
+  width: 5%;
 }
 
 </style>
